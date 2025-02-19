bf6e38da77e7beec70bbc2761347c16d2e432727
Matheus Henrique <matheushr39@gmail.com>
2025-02-19 17:33:20 -0300

    Teste 3.

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation'; // Atualize para next/navigation
+import HomeClient from '@/ContainerClient/Home';
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router) {
+      router.push('/home');
+    }
+  }, [router]);
+
+  return (
+    <>
+      <HomeClient />
+    </>
+  );
+}
